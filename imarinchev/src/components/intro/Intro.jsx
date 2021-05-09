e8559c8f8f0d358d4c76371b31e858068bdecccc
ivanmarinchev00 <ivanmarinchev00@gmail.com>
2021-05-09 17:23:04 +0200

    intro page complete

--- imarinchev/src/components/intro/Intro.jsx
+++ imarinchev/src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.css"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            Intro
+            <div className="introLeft">
+                <div className="imgContainer">
+                    <img src="assets/174608681_910871466416779_1360797091771506156_n.jpg"></img>
+                </div>
+            </div>
+            <div className="introRight">
+                <div className="introWrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Ivan Marinchev</h1>
+                    <h3>Frontend Developer</h3>
+                </div>
+                <a href="#portfolio" className="arrowContainer"> 
+                    <img src="assets/down.png" alt="" className="arrow"/>
+                </a>
+            </div>
         </div>
     )
 }
