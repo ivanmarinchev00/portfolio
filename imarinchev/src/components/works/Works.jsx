4fd0be10f5c093332f20471e771be7e66c3e4be0
ivanmarinchev00 <ivanmarinchev00@gmail.com>
2021-05-16 14:47:33 +0200

    experience slider

--- imarinchev/src/components/works/Works.jsx
+++ imarinchev/src/components/works/Works.jsx
@@ -1,27 +1,78 @@
 import "./works.css"
+import { useState, useEffect } from 'react';
 
 export default function Works() {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [currentLogo, setCurrentLogo] = useState("");
+    let logoSrc = "";
+
+    const data = [
+        {
+          id: "1",
+          logo: "../assets/reconart-social-logo.png",
+          title: "ReconArt",
+          role: "Frontend Developer Intern",
+          class: "reconart",
+          period: "03.2021-08.2021",
+          desc:
+            "Skills and Technologies: \n ES6, Ext Js, CSS Sprites, Code Quality and Performance, HTML, CSS",
+        },
+        {
+          id: "2",
+          logo: "../assets/fontys-logo.png",
+          title: "Fontys University of Applied Sciences",
+          role:"Student",
+          class: "fontys",
+          period: "08.2019-06.2023",
+          desc:
+            "Skills and Technologies: \n C#, PHP (Laravel), JavaScript (ES6), HTML, CSS, React Js, Swift UI, PWA, Kotlin, MySQL, Apache, GIT, Jira",
+        },
+        {
+          id: "3",
+          logo: "../assets/coming-soon.jpeg",
+          title: "Your company?",
+          period: "09.2021-02.2022",
+          desc:
+            "TBA",
+        },
+      ];
+
+      useEffect(()=> {
+            if (currentSlide === 1) setCurrentLogo("../assets/fontys-logo.png")
+            else if (currentSlide === 0) setCurrentLogo("../assets/reconart-social-logo.png");
+            else if (currentSlide === 2) setCurrentLogo("../assets/coming-soon.jpeg");
+      }, [currentSlide])
+
+
+      const handleClick = (way) => {
+        way === "left"
+          ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+          : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+      };
+    
+
     return (
         <div className="works" id="works">
-           <div className="slider">
-               <div className="slider-container">
-                   <div className="slider-item">
-                       <div className="slider-left">
-                           <img className="reconart" src="../assets/reconart-social-logo.png" alt="reconart logo"/>
+           <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+               {data.map((d) => (
+                   <div className="slider-container" key={d.id}>
+                   <div className="slider-item" >
+                       <div className="slider-left" >
+                           <img className={d.class} alt="logo" src={d.logo}/>
                        </div>
                        <div className="slider-right">
-                           <h2>ReconArt</h2>
-                           <h3>Frontend Developer Intern</h3>
-                            <h4>03.2021-08.2021</h4>
-                            <p>Skills and Technologies: <br/>
-                                ES6, Ext Js, CSS Sprites, Code Quality and Performance, HTML, CSS
+                           <h2>{d.title}</h2>
+                           <h3>{d.role}</h3>
+                            <h4>{d.period}</h4>
+                            <p className="new-line">{d.desc}
                             </p>
                        </div>
                    </div>
                </div>
+               ))}
            </div>
-           <img src="../assets/arrow.png" className="arrow-slider left"/>
-           <img src="../assets/arrow.png" className="arrow-slider right"/>
+           <img src="../assets/arrow.png" className="arrow-slider left" onClick={() => handleClick("left")} alt="left-arrow"/>
+           <img src="../assets/arrow.png" className="arrow-slider right"  onClick={() => handleClick()} alt="right-arrow"/>
         </div>
     )
 }
