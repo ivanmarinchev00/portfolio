e66c5b8679a65e713a2c1d0b224b30eae3ce71f4
ivanmarinchev00 <ivanmarinchev00@gmail.com>
2021-05-23 21:35:28 +0200

    contact page:

--- imarinchev/src/components/contact/Contact.jsx
+++ imarinchev/src/components/contact/Contact.jsx
@@ -4,7 +4,17 @@ import "./contact.css"
 export default function Contact() {
     return (
         <div className="contact" id="contact">
-            Contact
+            <div className="contactLeft">
+            <img src="assets/shake.svg" alt="" className="shake" />
+            </div>
+            <div className="contactRight">
+            <h2>Contact.</h2>
+            <form >
+                <input type="text" placeholder="email" />
+                <textarea placeholder="Message"></textarea>
+                <button type="submit">Send</button>
+            </form>
+            </div>
         </div>
     )
 }
